f37de1fd8d509d13abc32c77aa28a6a21b38d4d3
Victor Tolbert <victor.tolbert@gmail.com>
2019-10-21 08:42:29 -0400

    wip

---
+++ src/components/ColorPalettes.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="md-16">
+    <h2 class="text-2xl font-semibold">
+      Tailwind Color Palette
+    </h2>
+    <div class="mb-8">
+      <h3 class="text-xl">
+        Blues
+      </h3>
+      <div class="flex flex-wrap">
+        <div class="w-full lg:w-1/3 bg-blue-100 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          blue-100
+        </div>
+        <div class="w-full lg:w-1/3 bg-blue-200 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          blue-200
+        </div>
+        <div class="w-full lg:w-1/3 bg-blue-300 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          blue-300
+        </div>
+        <div class="w-full lg:w-1/3 bg-blue-400 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          blue-400
+        </div>
+        <div class="w-full lg:w-1/3 bg-blue-500 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          blue-500
+        </div>
+        <div class="w-full lg:w-1/3 bg-blue-600 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          blue-600
+        </div>
+        <div class="w-full lg:w-1/3 bg-blue-700 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          blue-700
+        </div>
+        <div class="w-full lg:w-1/3 bg-blue-800 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          blue-800
+        </div>
+        <div class="w-full lg:w-1/3 bg-blue-900 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          blue-900
+        </div>
+      </div>
+    </div>
+    <div class="mb-8">
+      <h3 class="text-xl">
+        Greens
+      </h3>
+      <div class="flex flex-wrap">
+        <div class="w-full lg:w-1/3 bg-green-100 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          green-100
+        </div>
+        <div class="w-full lg:w-1/3 bg-green-200 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          green-200
+        </div>
+        <div class="w-full lg:w-1/3 bg-green-300 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          green-300
+        </div>
+        <div class="w-full lg:w-1/3 bg-green-400 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          green-400
+        </div>
+        <div class="w-full lg:w-1/3 bg-green-500 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          green-500
+        </div>
+        <div class="w-full lg:w-1/3 bg-green-600 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          green-600
+        </div>
+        <div class="w-full lg:w-1/3 bg-green-700 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          green-700
+        </div>
+        <div class="w-full lg:w-1/3 bg-green-800 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          green-800
+        </div>
+        <div class="w-full lg:w-1/3 bg-green-900 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          green-900
+        </div>
+      </div>
+    </div>
+    <div class="mb-8">
+      <h3 class="text-xl">
+        Oranges
+      </h3>
+      <div class="flex flex-wrap">
+        <div class="w-full lg:w-1/3 bg-orange-100 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          orange-100
+        </div>
+        <div class="w-full lg:w-1/3 bg-orange-200 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          orange-200
+        </div>
+        <div class="w-full lg:w-1/3 bg-orange-300 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          orange-300
+        </div>
+        <div class="w-full lg:w-1/3 bg-orange-400 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          orange-400
+        </div>
+        <div class="w-full lg:w-1/3 bg-orange-500 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          orange-500
+        </div>
+        <div class="w-full lg:w-1/3 bg-orange-600 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          orange-600
+        </div>
+        <div class="w-full lg:w-1/3 bg-orange-700 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          orange-700
+        </div>
+        <div class="w-full lg:w-1/3 bg-orange-800 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          orange-800
+        </div>
+        <div class="w-full lg:w-1/3 bg-orange-900 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          orange-900
+        </div>
+      </div>
+    </div>
+    <div class="mb-8">
+      <h3 class="text-xl">
+        Reds
+      </h3>
+      <div class="flex flex-wrap">
+        <div class="w-full lg:w-1/3 bg-red-100 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          red-100
+        </div>
+        <div class="w-full lg:w-1/3 bg-red-200 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          red-200
+        </div>
+        <div class="w-full lg:w-1/3 bg-red-300 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          red-300
+        </div>
+        <div class="w-full lg:w-1/3 bg-red-400 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          red-400
+        </div>
+        <div class="w-full lg:w-1/3 bg-red-500 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          red-500
+        </div>
+        <div class="w-full lg:w-1/3 bg-red-600 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          red-600
+        </div>
+        <div class="w-full lg:w-1/3 bg-red-700 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          red-700
+        </div>
+        <div class="w-full lg:w-1/3 bg-red-800 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          red-800
+        </div>
+        <div class="w-full lg:w-1/3 bg-red-900 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          red-900
+        </div>
+      </div>
+    </div>
+    <div class="mb-8">
+      <h3 class="text-xl">
+        Grays
+      </h3>
+      <div class="flex flex-wrap">
+        <div class="w-full lg:w-1/3 bg-gray-100 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          gray-100
+        </div>
+        <div class="w-full lg:w-1/3 bg-gray-200 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          gray-200
+        </div>
+        <div class="w-full lg:w-1/3 bg-gray-300 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          gray-300
+        </div>
+        <div class="w-full lg:w-1/3 bg-gray-400 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          gray-400
+        </div>
+        <div class="w-full lg:w-1/3 bg-gray-500 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          gray-500
+        </div>
+        <div class="w-full lg:w-1/3 bg-gray-600 shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          gray-600
+        </div>
+        <div class="w-full lg:w-1/3 bg-gray-700 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          gray-700, hsl(218, 17%, 35%);
+        </div>
+        <div class="w-full lg:w-1/3 bg-gray-800 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          gray-800
+        </div>
+        <div class="w-full lg:w-1/3 bg-gray-900 text-white shadow py-4 px-8 w-40 text-sm mb-0 mr-0">
+          gray-900
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
